4c5f04ff82fed6148e0eb4e7df6242d2f03d47e2
Prateek Thakur <67188426+prateekthakur272@users.noreply.github.com>
2025-07-09 17:47:48 +0530

    adds more details

--- app/src/main/java/dev/prateekthakur/dragonballencyclopedia/ui/screens/characters/CharacterView.kt
+++ app/src/main/java/dev/prateekthakur/dragonballencyclopedia/ui/screens/characters/CharacterView.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,7 +15,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import dev.prateekthakur.dragonballencyclopedia.domain.models.DragonBallCharacter
